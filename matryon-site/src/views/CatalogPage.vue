0c0ebde86dd8bbf843693837598ed100e5224c2e
kiruhach <pochta23891@inbox.ru>
2026-06-18 17:36:46 +1000

    Fix buy button event handling on catalog page - use @click.stop modifier

--- matryon-site/src/views/CatalogPage.vue
+++ matryon-site/src/views/CatalogPage.vue
@@ -21,11 +21,9 @@ const lamps = ref([])
 const sockets = ref([])
 const loading = ref(true)
 
-async function handleBuy(e, productId) {
-  e.stopPropagation()
-  e.preventDefault()
+async function handleBuy(productId) {
   if (!user.value) {
-    router.push('/login?redirect=' + encodeURIComponent(router.currentRoute.value.fullPath))
+    router.push('/login?redirect=' + encodeURIComponent(route.fullPath))
     return
   }
   if (productId) {
@@ -75,7 +73,7 @@ onMounted(async () => {
               <h3 class="product-card__name">{{ item.name }}</h3>
               <p class="product-card__desc">{{ item.desc }}</p>
             </div>
-            <div class="product-card__action" @click="e => handleBuy(e, item.id)">
+            <div class="product-card__action" @click.stop="handleBuy(item.id)">
               <span>КУПИТЬ</span>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0020 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" fill="white"/>
@@ -96,7 +94,7 @@ onMounted(async () => {
               <h3 class="product-card__name">{{ item.name }}</h3>
               <p class="product-card__desc">{{ item.desc }}</p>
             </div>
-            <div class="product-card__action" @click="e => handleBuy(e, item.id)">
+            <div class="product-card__action" @click.stop="handleBuy(item.id)">
               <span>КУПИТЬ</span>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0020 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" fill="white"/>
@@ -117,7 +115,7 @@ onMounted(async () => {
               <h3 class="product-card__name">{{ item.name }}</h3>
               <p class="product-card__desc">{{ item.desc }}</p>
             </div>
-            <div class="product-card__action" @click="e => handleBuy(e, item.id)">
+            <div class="product-card__action" @click.stop="handleBuy(item.id)">
               <span>КУПИТЬ</span>
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49A1.003 1.003 0 0020 4H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" fill="white"/>
